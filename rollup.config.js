16def38f05b63cd6e73618aa8ef3e41ab2d83db4
qianjunjian <1018265043@qq.com>
2021-08-30 14:15:54 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -7,25 +7,30 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'build/index.cjs.js',
+      file: 'dist/index.cjs.js',
       format: 'cjs',
       name: "index"
     },
     {
-      file: 'build/index.es.js',
+      file: 'dist/index.es.js',
       format: 'es',
       name: "index"
     },
     {
-      file: 'build/index.umd.js',
+      file: 'dist/index.umd.js',
       format: 'umd',
       name: "index"
     },
   ],
-  external: [],
+  external: id => id.includes('@babel/runtime') || id.includes('core-js'),
   plugins: [
     typescript(),
     resolve(),
-    commonjs()
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',    // 防止打包node_modules下的文件
+      extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', "ts", "tsx"],
+      babelHelpers: "runtime"
+    }),
   ]
 };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-// import "babel-polyfill";
-interface IObj {
-    [key: string]: any;
+function aa () {
+    return new Promise((resolve, reject) => {
+        resolve(11)
+    })
 }
 
-export default function encodeUrlParam(a: IObj): string {
-    const arr: string[] = [];
-    let key = "";
-    for (key in a) {
-        arr.push(encodeURIComponent(key) + "=" + encodeURIComponent(a[key]));
-    }
-    return arr.join("&").replace(/%20/g, "+");
-}
+export default aa;
